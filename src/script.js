dfbb3dafb1c4eeb460b4e9414e82b7a00826b0a2
Zelalem <mekonnen.zelalem251@gmail.com>
2022-02-03 19:23:54 +0100

    fix linter errors

--- src/script.js
+++ src/script.js
@@ -1,57 +1,57 @@
-console.log("HI");
-const mobileMenu = document.querySelector(".mobile-menu");
-const hamburgerIcon = document.querySelector(".header__hamurger--menu-icon");
+console.log('HI');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerIcon = document.querySelector('.header__hamurger--menu-icon');
 const gustInstructors = [
   {
-    image: "../assets/member1.png",
-    name: "Yochai Benkler",
+    image: '../assets/member1.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
   {
-    image: "../assets/member2.png",
-    name: "Yochai Benkler",
+    image: '../assets/member2.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
   {
-    image: "../assets/member3.png",
-    name: "Yochai Benkler",
+    image: '../assets/member3.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
   {
-    image: "../assets/member4.png",
-    name: "Yochai Benkler",
+    image: '../assets/member4.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
   {
-    image: "../assets/member5.png",
-    name: "Yochai Benkler",
+    image: '../assets/member5.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
   {
-    image: "../assets/member6.png",
-    name: "Yochai Benkler",
+    image: '../assets/member6.png',
+    name: 'Yochai Benkler',
     position:
-      "Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School",
+      'Berkman Professonr of Enterpreneurial Legal Studies at Harvard Law School',
     describtion:
-      "Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.",
+      'Benkler studies commons-based peer poduction,and published his seminal book. The Wealthof Networks in 2006.',
   },
 ];
-const featuredSpeaker = document.querySelector(".featured-speakers");
+const featuredSpeaker = document.querySelector('.featured-speakers');
 
 mobileMenu.innerHTML = ` <ul class="mobile-nav">
         <li><i class="fas fa-window-close x-icon"></i></li>
@@ -64,22 +64,22 @@ mobileMenu.innerHTML = ` <ul class="mobile-nav">
       </ul>`;
 const displayMenu = (event) => {
   if (event.target === hamburgerIcon) {
-    mobileMenu.classList.toggle("display-none");
+    mobileMenu.classList.toggle('display-none');
   }
-  window.document.querySelector("body").classList.toggle("no-scroll");
+  window.document.querySelector('body').classList.toggle('no-scroll');
 };
-const xIcon = document.querySelector(".x-icon");
+const xIcon = document.querySelector('.x-icon');
 
 const closeMenu = (event) => {
   if (event.target === xIcon) {
-    mobileMenu.classList.toggle("display-none");
+    mobileMenu.classList.toggle('display-none');
   }
-  window.document.querySelector("body").classList.toggle("no-scroll");
+  window.document.querySelector('body').classList.toggle('no-scroll');
 };
 
 const addFeaturedSpeaker = () => {
   for (let i = 0; i <= gustInstructors.length; i += 1) {
-    const featuredInstructor = document.createElement("div");
+    const featuredInstructor = document.createElement('div');
 
     featuredInstructor.innerHTML = `
   <img src='${gustInstructors[i].image}' alt="" class="featured-image">
@@ -92,33 +92,31 @@ const addFeaturedSpeaker = () => {
 
     featuredSpeaker.appendChild(featuredInstructor);
     if (i >= 2) {
-      featuredInstructor.classList.add("more");
+      featuredInstructor.classList.add('more');
     }
   }
 };
 
-const seeMoreBtn = document.querySelector("#see-more");
-const more = document.querySelector(".more");
+const seeMoreBtn = document.querySelector('#see-more');
 const seeMore = (event) => {
   if (window.screen > 768) {
     for (let i = 4; i < 6; i += 1) {
-      featuredSpeaker.childNodes[i].classList.toggle("dn-feature");
+      featuredSpeaker.childNodes[i].classList.toggle('dn-feature');
     }
   } else {
     for (let i = 2; i < 6; i += 1) {
-      featuredSpeaker.childNodes[i].classList.toggle("dn-feature");
+      featuredSpeaker.childNodes[i].classList.toggle('dn-feature');
     }
   }
 
-  console.log(event.target.innerHTML);
-  if (event.target.innerHTML === "LESS") {
-    seeMoreBtn.innerHTML = "MORE";
+  if (event.target.innerHTML === 'LESS') {
+    seeMoreBtn.innerHTML = 'MORE';
   } else {
-    seeMoreBtn.innerHTML = "LESS";
+    seeMoreBtn.innerHTML = 'LESS';
   }
 };
 
-hamburgerIcon.addEventListener("click", displayMenu);
-xIcon.addEventListener("click", closeMenu);
-window.addEventListener("load", addFeaturedSpeaker);
-seeMoreBtn.addEventListener("click", seeMore);
+hamburgerIcon.addEventListener('click', displayMenu);
+xIcon.addEventListener('click', closeMenu);
+window.addEventListener('load', addFeaturedSpeaker);
+seeMoreBtn.addEventListener('click', seeMore);
